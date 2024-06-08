dc8111cfeeebef0bdd4d46854f58c2f93d81436a
Hazley Jarquin <hazleyjarquin03@gmail.com>
2024-06-08 16:10:26 -0600

    configuring shadcn.ui to the library

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,4 @@
+import { Button as ShadcnButton } from "@/components/ui/button";
 import React from "react";
 
 interface ButtonProps {
@@ -6,7 +7,7 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+  return <ShadcnButton onClick={onClick}>{label}</ShadcnButton>;
 };
 
 export default Button;
